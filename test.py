2ccd9bd6a8799f9d461073ebdb2779522bf8c805
SUN-JY <forits@qq.com>
2020-11-04 19:43:17 +0800

    2020年11月4日19点43分

--- test.py
+++ test.py
@@ -1,24 +1,7 @@
-#!/usr/bin/python3
+import include.log as Log
 
-import _thread
-import time
+if __name__=="__main__":
+    Log.hi()
 
-
-# 为线程定义一个函数
-def print_time(threadName, delay):
-    count = 0
-    while count < 5:
-        time.sleep(delay)
-        count += 1
-        print("%s: %s" % (threadName, time.ctime(time.time())))
-
-
-# 创建两个线程
-try:
-    _thread.start_new_thread(print_time, ("Thread-1", 2,))
-    _thread.start_new_thread(print_time, ("Thread-2", 4,))
-except:
-    print("Error: 无法启动线程")
-
-while 1:
-    pass
+    while 1:
+        pass
